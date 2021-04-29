8d0d8d05aff6a63837fec460bd8dc45ba27c1cb5
mikewichmann21 <83356647+mikewichmann21@users.noreply.github.com>
2021-04-29 11:11:12 +0200

    RPA Basics

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://0Z8GIOAjr7PvgEYfDHsu0iofh9v3naeG_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Grundlagen Robotic Process Automation"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Sich wiederholende Aufgaben am PC nerven dich oder deine Kollegen? Musst du oder dein Team regelm&amp;auml;&amp;szlig;ig manuell Daten von einem System in ein anderes &amp;uuml;bertragen? Mithilfe von Robotic Process Automation werden diese Art von Aufgaben von Robots ausgef&amp;uuml;hrt werden. Ihr k&amp;ouml;nnt euch somit endlich auf sinnvolle, kreative oder emotionale Aufgaben fokussieren. Der Kurs RPA f&amp;uuml;r Einsteiger richtet sich an alle, die sich mit dem Thema RPA besch&amp;auml;ftigen wollen und erste Grundkenntnisse in der Praxis anwenden wollen.&amp;nbsp;&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;ul&gt;&lt;li&gt;Du lernst, was Robotic Process automation ist und wie es dir und deinem Unternehmen helfen kann effizienter zu sein.&lt;/li&gt;&lt;li&gt;Wir geben dir einen &amp;uuml;berblick &amp;uuml;ber state of the Art Tools und die aktuelle Marktsituation f&amp;uuml;r RPA&lt;/li&gt;&lt;li&gt;Du lernst wie Profis ein RPA Projekt in Unternehmen implementieren und welche Rollen es in solchen Projekten gibt&lt;/li&gt;&lt;li&gt;Schlie&amp;szlig;lich wirst du deine erste eigene Automatisierung umsetzen und feststellen, dass es leichter ist als du dachtest.&lt;/li&gt;&lt;/ul&gt;&lt;p&gt;Jedes Modul endet mit einer Zusammenfassung und einem Wissenscheck. Insgesamt ben&amp;ouml;tigst du inklusive Praxisprojekt circa 8 Stunden, um den Kurs zu absolvieren.&lt;/p&gt;"
+};
